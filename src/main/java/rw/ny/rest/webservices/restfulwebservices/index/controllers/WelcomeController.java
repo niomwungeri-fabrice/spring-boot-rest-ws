1e83b3442694727570a5e13daebd9222f4c0fb35
Fabrice Niyomwungeri <niomwungeri.fabrice@gmail.com>
2023-09-21 18:02:33 +0200

    add i18n

--- src/main/java/rw/ny/rest/webservices/restfulwebservices/index/controllers/WelcomeController.java
+++ src/main/java/rw/ny/rest/webservices/restfulwebservices/index/controllers/WelcomeController.java
@@ -1,13 +1,25 @@
 package rw.ny.rest.webservices.restfulwebservices.index.controllers;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rw.ny.rest.webservices.restfulwebservices.dto.index.WelcomeDTO;
 
+import java.util.Locale;
+
 @RestController
 public class WelcomeController {
+
+    private final MessageSource messageSource;
+
+    public WelcomeController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/index")
     public WelcomeDTO welcomeToMicroservice() {
-        return new WelcomeDTO("Welcome to Spring microservices");
+        Locale locale = LocaleContextHolder.getLocale();
+        return new WelcomeDTO(messageSource.getMessage("good.morning.message", null, "Default Message", locale));
     }
 }
